8759f769f9684dce7ee1c74a587548f211aa62ef
Fred <higgins.fred.s@gmail.com>
2022-04-26 10:56:48 -0700

    commit tree 9af8e667297c81f917d5ad982345f16ce52fe317

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        list = []
+        self.characters.each do |character|
+           list << "#{character.name} - #{character.show.name}"
+        end
+        list
+    end
+
 end
